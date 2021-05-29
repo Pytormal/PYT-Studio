eb5e070a1d8db4b9bfa58201fdfdd29c72710715
AlexAndrew Mallory <pytormal@live.com>
2021-05-28 18:08:32 -0700

    removed Hub from Polls Hub

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-import './styles/timer.css'
-
-
+import "./styles/timer.css";
 
 class Home extends React.Component {
-state = {
-  user: {user_name: "Alex Andrew"}
-}
+  state = {
+    user: { user_name: "Alex Andrew" },
+  };
 
   render() {
     // let user = JSON.parse(sessionStorage.getItem('userName'))
@@ -24,7 +21,7 @@ state = {
 
           <div className="home-links links">
             <Link to="/login">Login</Link>
-            <Link to="/Polls-hub">Polls Hub</Link>
+            <Link to="/Polls-hub">Polls</Link>
             <Link to="/upload-song">Upload Songs here</Link>
             <Link to="/timers">Timers</Link>
           </div>
