eaf45dbf48d98ba5582ffb3c9da24aa7b1b6a9d0
Alex Andrew Mallory <59344593+Pytormal@users.noreply.github.com>
2023-02-17 17:29:26 -0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,16 +15,16 @@ class Home extends React.Component {
         <div className="title-card">
           <section className="welcome-title">
             <h1> Welcome to Pytormal's Studio</h1>
-       {/*  //  <span id="WelcomeUser">{`${this.state.user.user_name},`}</span>
-         //   <p className="home-p">what would you like to do?</p> */} 
+       {/*  <span id="WelcomeUser">{`${this.state.user.user_name},`}</span>
+          <p className="home-p">what would you like to do?</p> */} 
 <h2> a collection of Alex Andrew's art</h2>
           </section>
 
           <div className="home-links links">
             <Link to="/login">Login</Link>
-        {/× //    <Link to="/Polls-hub">Polls</Link>
-            {/* <Link to="/upload-song">Upload Songs here</Link> */}
-            <Link to="/timers">Timers</Link> ×/} 
+        {/* <Link to="/Polls-hub">Polls</Link>
+             <Link to="/upload-song">Upload Songs here</Link>
+            <Link to="/timers">Timers</Link> */} 
             <Link to="video-gallery"> Videos</Link>
           </div>
         </div>
