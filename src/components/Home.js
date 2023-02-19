c35b9d3174c01416f2d4188a5ff95e74605fa65f
Alex Andrew Mallory <Pytormal@live.com>
2023-02-18 21:38:37 -0800

    added text, changed css

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,16 +15,25 @@ class Home extends React.Component {
         <div className="title-card">
           <section className="welcome-title">
             <h1> Welcome to Pytormal's Studio</h1>
-       {/*  <span id="WelcomeUser">{`${this.state.user.user_name},`}</span>
-          <p className="home-p">what would you like to do?</p> */} 
-<h2> a collection of Alex Andrew's art</h2>
+            {/*  <span id="WelcomeUser">{`${this.state.user.user_name},`}</span>
+          <p className="home-p">what would you like to do?</p> */}
+            <h2> a collection of Alex Andrew's art</h2>
+
+            <p Id="placeholder">
+              Web Page will change due to many variables, some links may not
+              work or pages have no relevant data or forms that relate to cinema
+              or photograpghy. Layout and Responsive designs will change overtime.
+              <p id="parkey">
+                this was a former webapp that was created for overlays for live streaming and recording.
+              </p>
+            </p>
           </section>
 
           <div className="home-links links">
             <Link to="/login">Login</Link>
-        {/* <Link to="/Polls-hub">Polls</Link>
+            {/* <Link to="/Polls-hub">Polls</Link>
              <Link to="/upload-song">Upload Songs here</Link>
-            <Link to="/timers">Timers</Link> */} 
+            <Link to="/timers">Timers</Link> */}
             <Link to="/video-gallery">Videos</Link>
           </div>
         </div>
