f80b69ffc26130381f88b70e55554aa30e0e9ccd
Alex Andrew Mallory <59344593+Pytormal@users.noreply.github.com>
2023-03-08 15:05:19 -0800

    Create Photos.js

---
+++ src/components/Photos.js
@@ -0,0 +1,38 @@
+import React from "react";
+// import MessageForm from "../forms/MessageForm";
+
+class Photogallery extends React.Component {
+  // state = {
+  //   user: { user_name: "Alex Andrew" },
+  //   message: "",
+  // };
+
+  // sendMessage = (formData) => {
+  //   const newMessage = {
+  //     id: Date.now(),
+  //     user: this.user_name,
+  //     message: formData.message,
+  //   };
+  // };
+
+  render() {
+    return (
+      <>
+        <section id="streaming">
+          <section id="split-stream-comments">
+            <section className="stream-video">
+              <p> page in progress </>
+              
+ 
+            </section>
+            {/* <section id="single">
+              <MessageForm />
+            </section> */}
+          </section>
+        </section>
+      </>
+    );
+  }
+}
+
+export default Photogallery;
