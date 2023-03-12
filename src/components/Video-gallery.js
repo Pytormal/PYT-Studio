771977504c237bd820817cdb3c955e0c064c4b35
Alex Andrew Mallory <Pytormal@live.com>
2023-03-12 16:39:13 -0700

    Update Video-gallery.js

--- src/components/Video-gallery.js
+++ src/components/Video-gallery.js
@@ -21,9 +21,15 @@ class Videogallery extends React.Component {
         <section id="streaming">
           <section id="split-stream-comments">
             <section className="stream-video">
-              
-              <iframe width="560"
-height="315" src="https://www.youtube.com/embed/1_K_QsMvzVY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/fzfF3t8APts"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
 
               <iframe
                 width="756"
