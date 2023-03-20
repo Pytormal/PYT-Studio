265173f0da084a81db1c74906e83f67c94a37d47
Alex Andrew Mallory <59344593+Pytormal@users.noreply.github.com>
2023-03-20 05:47:39 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,14 +21,14 @@ class Home extends React.Component {
         <h2>My Projects</h2>
 
         <div id="myCarousel" class="carousel slide" data-ride="carousel">
-          <!-- Indicators -->
+
           <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
             <li data-target="#myCarousel" data-slide-to="1"></li>
             <li data-target="#myCarousel" data-slide-to="2"></li>
           </ol>
 
-          <!-- Wrapper for slides -->
+         
           <div class="carousel-inner">
             <div class="item active">
               <a
@@ -44,7 +44,7 @@ class Home extends React.Component {
             </div>
           </div>
 
-          <!-- Left and right controls -->
+         
           <a class="left carousel-control" href="#myCarousel" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left"></span>
             <span class="sr-only">Previous</span>
