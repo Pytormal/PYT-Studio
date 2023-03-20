53f54519531e03237f83a62e63adf0bd74f797d6
Alex Andrew Mallory <59344593+Pytormal@users.noreply.github.com>
2023-03-20 05:43:27 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,6 +17,71 @@ class Home extends React.Component {
             <h1> Welcome to Pytormal's Studio</h1>
             {/*  <span id="WelcomeUser">{`${this.state.user.user_name},`}</span>
           <p className="home-p">what would you like to do?</p> */}
+<div class="container">
+        <h2>My Projects</h2>
+
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner">
+            <div class="item active">
+              <a
+                href="other pages/Project 1/Build Week Unit1/marketing_page/Index.html"
+                ><img src="https://picsum.photos/id/103/367/267" alt="1" />
+              </a>
+            </div>
+
+            <div class="item">
+              <a href=""
+                ><img src="https://picsum.photos/id/104/367/267" alt="2"
+              /></a>
+            </div>
+          </div>
+
+          <!-- Left and right controls -->
+          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a
+            class="right carousel-control"
+            href="#myCarousel"
+            data-slide="next"
+          >
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+
+      <section class="horizontal-scroll">
+        <section class="top-row">
+          <ul>
+            <li>
+             
+             
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/mYAAy2D3Yoc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                
+             
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/id/104/367/267" alt="2" />
+              <h3>
+                <a href="other pages/Project 2.html">Project 2</a>
+              </h3>
+            </li>
+          </ul>
+ 
+        </section>
+      </section>
             <section class="featured">
               <h2 id="collection"> a collection of Alex Andrew's art</h2>
               <h4>featured video</h4>
